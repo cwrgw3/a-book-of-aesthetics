a985c96e0392dadfb657e7198a933fe6d16e7d91
LSE77 <seokhwan.lee.s@gmail.com>
2025-05-21 14:31:23 +0900

    애니메이션 하다맘
    ㅅㄱ

--- A book of aesthetics/Assets/scripts/player.cs
+++ A book of aesthetics/Assets/scripts/player.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     Rigidbody2D rigid;
+    Animator anim;
+
     public float moveSpeed = 10f;
     public float jumpPower = 12f;
 
-    bool isGrounded = false; // 땅에 닿아 있는지 여부
+    bool isGrounded = false;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -23,21 +26,30 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
-        // Space 키를 눌렀고, 땅에 있을 때만 점프
+        float h = Input.GetAxisRaw("Horizontal");
+
+        // ▶ ① 이동 시 애니메이션
+        anim.SetFloat("Speed", Mathf.Abs(h));
+
+        // ▶ ② 좌우 방향에 따라 캐릭터 반전 (선택사항)
+        if (h != 0)
+            transform.localScale = new Vector3(h > 0 ? 1 : -1, 1, 1);
+
+        // ▶ ③ 점프 입력 처리
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-            isGrounded = false; // 점프했으므로 false
+            isGrounded = false;
+            anim.SetBool("IsJumping", true);
         }
     }
 
-    // 바닥 충돌 체크
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // "Ground"라는 태그가 붙은 오브젝트와 충돌하면 땅에 있는 것으로 판단
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            anim.SetBool("IsJumping", false);
         }
     }
 }
